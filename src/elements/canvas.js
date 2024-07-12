ed3b38a47130b4ea8b6f550eeb43a625b447f4a1
John Weber <john.weber@swimlane.com>
2024-07-12 11:07:07 -0600

    30

--- src/elements/canvas.js
+++ src/elements/canvas.js
@@ -3,12 +3,7 @@ import {repeat} from 'lit/directives/repeat.js';
 import { SwdkUtilities } from '../utilities/utilities.js';
 
 /**
- * Canvas takes one input, contents, which must always be an array of JSON dict
- * objects. Much like the Swimlane App-Builder, the arrangement of the contents 
- * of a Canvas are completely determined by the order in which they are 
- * rendered and their relative width. And just like the Swimlane App-Builder,
- * this can been further adjusted using the organizational components Section
- * and Tabs.
+ * 30
 */
 
 export class SwdkCanvas extends LitElement {
