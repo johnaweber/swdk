6eb394ac095df1bd88cb6100626cf0e3a5816afd
John A. Weber <john.weber@swimlane.com>
2025-04-01 11:23:27 -0600

    consolidate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-const SwdkCanvas = require('./elements/canvas');
-const SwdkTextField = require('./elements/text-field');
-const SwdkUtilities = require('./utilities/utilities');
 
-module.exports = [
-    SwdkCanvas,
-    SwdkTextField,
-    SwdkUtilities
-]
+import { Textbox } from './elements/text-box.js';
+
+export { Textbox };
